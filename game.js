9a098cfba37405e9b4eeb3aa019143ac4d07db5f
Krishang Mittal <89867429+krishangMittal@users.noreply.github.com>
2023-07-23 14:58:12 +0530

    Update game.js

--- game.js
+++ game.js
@@ -5,6 +5,7 @@ var userClickedPattern = [];
 var started = false;
 var level = 0;
 
+//1. Use jQuery to detect when a keyboard key has been pressed, when that happens for the first time, call nextSequence().
 $(document).keypress(function() {
   if (!started) {
     $("#level-title").text("Level " + level);
@@ -13,39 +14,73 @@ $(document).keypress(function() {
   }
 });
 
-
 $(".btn").click(function() {
+
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
   playSound(userChosenColour);
+
   animatePress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length-1);
+
+
 });
 
+
+function checkAnswer(currentLevel) {
+
+    if (gamePattern[currentLevel] == userClickedPattern[currentLevel]) { 
+
+        console.log("Success");
+
+        if (userClickedPattern.length === gamePattern.length){
+            setTimeout(function () {
+              nextSequence();
+            }, 1000);
+        }
+
+    } else {
+        playSound("wrong");
+        $("body").addClass("game-over");
+        setTimeout(function() {
+            $("body").removeClass("game-over");
+        },200);
+
+         $("#level-title").text("Game Over, JAS RANDI"); 
+         startOver();
+    }
+
+}
+
+
 function nextSequence() {
+  userClickedPattern = [];
   level++;
-  $("level-title").text("Level " + level);
+  $("#level-title").text("Level " + level);
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
+
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
   playSound(randomChosenColour);
-
 }
 
 function playSound(name) {
-    var audio = new Audio("sounds/" + name + ".mp3");
-    audio.play();
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
 
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
 }
 
 
-function animatePress(currentColour) {
-    $("#" + currentColour).addClass("pressed");
-
-    setTimeout(function () {
-        $("#" + currentColour).removeClass("pressed");
-      }, 100);
-    }
-    
- 
-  
+function startOver() { 
+    level = 0;
+    gamePattern = [];
+    started = false;
+}
